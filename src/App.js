fe7cea3b25492b2616970ca96ef4ac6ca687b96e
Migyas <yansantos0712@gmail.com>
2021-10-21 22:43:36 -0300

    Aula De hoje 22:43

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [pokemon, setPokemon] = useState({});
+
+  const showPokemon = async () => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`);
+    const data = await response.json();
+    setPokemon(data);
+  };
+
+  useEffect(() => {
+    showPokemon();
+  }, []);
+
+  return (
+    <>
+      <h1>{pokemon && pokemon.name?.toUpperCase()}</h1>
+      <img
+        src={pokemon && pokemon.sprites?.other.dream_world.front_default}
+        alt="pokemon img"
+      />
+    </>
+  );
+}
+
+export default App;
